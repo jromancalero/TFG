2c34ec95a5fa6c7bdbb066ef945b696297db1c37
jorge <jromancalero@gmail.com>
2022-05-18 11:32:14 +0200

    arrgeglo

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -25,7 +25,7 @@
             <h2 class="h2__titulo_producto">{{$comidas[$i]->name}}</h2>
             <p class="p__description">{{$comidas[$i]->description}}</p>
             <p class="p__price">{{$comidas[$i]->price}} €</p>
-            <button class="boton__compra animacion">Añadir a la cesta</button>sf
+            <button class="boton__compra animacion">Añadir a la cesta</button>
         </article>
         @endfor
         {{-- BOTON DE VER MAS Y REDIRECCION --}}
