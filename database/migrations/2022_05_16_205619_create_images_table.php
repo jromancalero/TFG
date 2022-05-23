7cefb45d6839cc37360187925fcf26205edace23
jorge <jromancalero@gmail.com>
2022-05-23 13:46:07 +0200

    Añadido metodos delete a falta de arreglas el onCascade

--- database/migrations/2022_05_16_205619_create_images_table.php
+++ database/migrations/2022_05_16_205619_create_images_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('url');
-            $table->foreignId('product_id')->constrained();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
